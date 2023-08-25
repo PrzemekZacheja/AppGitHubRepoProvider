7300beb1277118dc122e2c280b015129b06bf713
Przemysław Zacheja <przemek.zacheja@gmail.com>
2023-08-25 14:18:10 +0200

    First commit

---
+++ src/main/java/com/example/appgithubrepo/AppGitHubRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.appgithubrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGitHubRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppGitHubRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appgithubrepo/AppGitHubRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appgithubrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGitHubRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
